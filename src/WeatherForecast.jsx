3754340fab7a5c93821e2449606df4b053ace20f
Callum Inglis <callum.inglis@4oh4.co.uk>
2024-06-18 11:41:08 +0100

    WeatherData component

--- src/WeatherForecast.jsx
+++ src/WeatherForecast.jsx
@@ -2,18 +2,35 @@ import './WeatherForecast.css'
 
 const WeatherIcon = (props) => {
     return (
-        <img src={props.img} alt={props.imgAlt} />
+        <>
+            <img src={props.img} alt={props.imgAlt} />
+        </>
+    )
+}
+
+const WeatherData = (props) => {
+    return (
+        <>
+            <h2>{props.day}</h2>
+            <p><span>conditions: </span> {props.conditions}</p>
+            <p><span>time: </span> {props.time}</p>
+        </>
     )
 }
 
 const WeatherForceast = (props) => {
     return (
         <div className="weather">
-            <h2>Day of the Week</h2>
-            <img src={props.img} alt={props.imgAlt} />
-            <WeatherIcon src={props.img} alt={props.imgAlt} />
-            <p><span>conditions: </span> {props.conditions}</p>
-            <p><span>time: </span> {props.time}</p>
+            <WeatherIcon 
+                img={props.img} 
+                imgAlt={props.imgAlt} 
+            />
+
+            <WeatherData 
+                day={props.day} 
+                conditions={props.conditions} 
+                time={props.conditions} 
+            />
         </div>
     )
 }
